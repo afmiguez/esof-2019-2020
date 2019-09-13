81cf98be1b6590c6e25c16b5cb4e90570a81b331
Alessandro Miguez <afmiguez@gmail.com>
2019-09-13 15:39:37 +0100

    first commit

---
+++ src/main/java/edu/ufp/afmiguez/tk/esof/EsofApplication.java
@@ -0,0 +1,13 @@
+package edu.ufp.afmiguez.tk.esof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsofApplication.class, args);
+    }
+
+}
